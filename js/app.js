52ec0a9d91c3ed073832459cb8372f0ecb96df41
AnastasiaArt <art-nastya@inbox.ru>
2018-09-05 00:43:16 +0300

    добавление элементов

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var taskList=document.querySelector('ul');
+function addTodo() {
+  var taskItem = document.createElement('li');
+  var inputValue = document.getElementById('task-id').value;
+  var task = document.createTextNode(inputValue);
+  taskItem.appendChild(task);
+  taskList.appendChild(taskItem);
+  document.getElementById('task-id').value = "";
+  var doneButton= document.createElement('button');
+  doneButton.className = "done button";
+  var editButton= document.createElement('button');
+  editButton.className = "edit button";
+  var deleteButton= document.createElement('button');
+  deleteButton.className = "delete button";
+  taskItem.appendChild(doneButton);
+  taskItem.appendChild(editButton);
+  taskItem.appendChild(deleteButton);
+}
