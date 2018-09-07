103774e0c2e62c5a66243c1947d793443f8215af
AnastasiaArt <art-nastya@inbox.ru>
2018-09-08 00:13:46 +0300

    done, delete

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
-var taskList=document.querySelector('ul');
+function deleteTodo() {
+  var taskItem = this.parentNode;
+  var taskList = taskItem.parentNode;
+
+  taskList.removeChild(taskItem);
+
+}
+function doneTodo(){
+  var taskItem = this.parentNode;
+  var taskList = taskItem.parentNode;
+  taskList.classList.add('checked');
+}
+function delInput() {
+  var inputValue = document.getElementById('task-id');
+  document.getElementById('task-id').value = "";
+  inputValue.focus();
+}
 function addTodo() {
+  var taskList = document.querySelector('ol');
   var taskItem = document.createElement('li');
-  var inputValue = document.getElementById('task-id').value;
-  var task = document.createTextNode(inputValue);
+  var inputValue = document.getElementById('task-id');
+  var task = document.createElement('b');
+  task.innerText = inputValue.value;
   taskItem.appendChild(task);
-  if(inputValue == "") {
+  if(inputValue.value == "") {
       alert("Введите задачу!");
+      inputValue.focus();
    } else {
       taskList.appendChild(taskItem);
    }
@@ -16,7 +35,13 @@ function addTodo() {
   editButton.className = "edit button";
   var deleteButton= document.createElement('button');
   deleteButton.className = "delete button";
+
   taskItem.appendChild(doneButton);
   taskItem.appendChild(editButton);
   taskItem.appendChild(deleteButton);
+
+  deleteButton.addEventListener('click',deleteTodo);
+  doneButton.addEventListener('click',doneTodo);
+
+  inputValue.focus();
 }
